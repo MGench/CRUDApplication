d940ca01b835880718a4bb34d3646f098c8e5ed4
mustafa.genc@hotmail.co.uk <mustafa.genc@hotmail.co.uk>
2024-07-23 13:50:39 +0100

    Created a database file

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 
 namespace CRUDApplication
 {
@@ -13,5 +15,14 @@ namespace CRUDApplication
     /// </summary>
     public partial class App : Application
     {
+        // this method executes everytime the application is started
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //creates databasefile if non existent
+            //Adds the tables specified in DataSession class
+            DatabaseFacade facade = new DatabaseFacade(new DataSession()); 
+            facade.EnsureCreated(); // ensures database file is created
+        }
+
     }
 }
