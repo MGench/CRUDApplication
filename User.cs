b2b887f53f3b145d2536043588fea30d87489aa8
mustafa.genc@hotmail.co.uk <mustafa.genc@hotmail.co.uk>
2024-07-23 13:41:58 +0100

    Created datatable fields

--- User.cs
+++ User.cs
@@ -1,10 +1,16 @@
-﻿namespace CRUDApplication
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDApplication
 {
     public class User
     {
+        //defining fields of the datatable
+        [Key] // the primary key is Id
+        public int Id { get; set; }
 
+        public string Name { get; set; }
 
-
+        public string Address { get; set; }
 
     }
 }
